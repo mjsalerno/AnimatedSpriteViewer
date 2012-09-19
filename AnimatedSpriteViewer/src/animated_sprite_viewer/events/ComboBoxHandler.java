3b04ef10e79a18d7bacff9b38c1459b21da57726
Michael Salerno <roofis20002003@yahoo.com>
2012-09-18 20:26:38 -0400

    added comments to the ComboBoxHandler

--- AnimatedSpriteViewer/src/animated_sprite_viewer/events/ComboBoxHandler.java
+++ AnimatedSpriteViewer/src/animated_sprite_viewer/events/ComboBoxHandler.java
@@ -9,28 +9,34 @@ import sprite_renderer.AnimationState;
 import sprite_renderer.Sprite;
 
 /**
- * The StartAnimationHandler class responds to when the user
- * requests to start animation.
- * 
- * @author  Richard McKenna
- *          Debugging Enterprises
- * @version 1.0
+ * handles all of the events thrown by the combo box.
+ *
+ * @author Michael
  */
-public class ComboBoxHandler implements ActionListener
-{
+public class ComboBoxHandler implements ActionListener {
+
     private JComboBox box;
     private ArrayList<Sprite> sprites;
-    
-    
-    public ComboBoxHandler(JComboBox box, ArrayList<Sprite> sprites)
-    {
+
+    /**
+     * a full constructor.
+     *
+     * @param box the JComboBox that will throw events.
+     * @param sprites an ArrayList that has the sprite the SceneRenderer is
+     * drawing
+     */
+    public ComboBoxHandler(JComboBox box, ArrayList<Sprite> sprites) {
         this.box = box;
         this.sprites = sprites;
-    }    
+    }
 
-        @Override
+    /**
+     * changes the animation state of the Sprite to the one selected in the combo box
+     * @param ae the ActionEvent that was thrown
+     */
+    @Override
     public void actionPerformed(ActionEvent ae) {
-        if (box != null && box.getItemAt(box.getSelectedIndex()) != null &&!AnimatedSpriteViewer.SELECT_ANIMATION_TEXT.equals(box.getItemAt(box.getSelectedIndex()).toString())) {
+        if (box != null && box.getItemAt(box.getSelectedIndex()) != null && !AnimatedSpriteViewer.SELECT_ANIMATION_TEXT.equals(box.getItemAt(box.getSelectedIndex()).toString())) {
             sprites.get(0).setAnimationState(AnimationState.valueOf(box.getItemAt(box.getSelectedIndex()).toString()));
         }
 
